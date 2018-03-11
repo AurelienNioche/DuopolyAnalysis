14d91d13df533afe6ca8ec7c7e2fdb87faa8864c
AurelienNioche <nioche.aurelien@gmail.com>
2018-03-11 15:25:19 +0100

    update

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ DATABASES = {
     },
     'old': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'duopolyNew.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'duopolyOld.sqlite3'),
     }
 }
 
