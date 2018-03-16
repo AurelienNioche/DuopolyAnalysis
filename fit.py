3f1ee9e0847315c382f7d127fc99397dd306b9f5
AurelienNioche <nioche.aurelien@gmail.com>
2018-03-16 14:51:34 +0100

    update

--- fit.py
+++ fit.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec
 import itertools as it
 # from hyperopt import fmin, tpe, hp
-from scipy.stats import mannwhitneyu
+import scipy.stats
+import statsmodels.stats.multitest
 
 from backup import backup
 from model import fit
@@ -122,15 +123,7 @@ def get_fit(force):
     return fit_backup
 
 
-def main(force, do_it_again):
-
-    if not os.path.exists("data/fit.p") or do_it_again or force:
-        fit_b = get_fit(force)
-
-    else:
-        fit_b = backup.load("data/fit.p")
-
-    # --------- ind plot
+def ind_plots(fit_b):
 
     scores_to_plot = ["profit", "competition"]  # fit.Score.names
     n_dim = len(scores_to_plot)
@@ -162,44 +155,24 @@ def main(force, do_it_again):
                               title=title, colors=colors,
                               n_dim=n_dim, n_cols=20)
 
-    # ------------------------------------ #
 
-    # colors = np.array(["C0" if i == 0.25 else "C1" for i in fit_b.r])
-    # markers = np.array(["o" if i else "x" for i in fit_b.display_opponent_score])
-    #
-    # x = fit_b.fit_scores["profit"]
-    # y = fit_b.fit_scores["competition"]
-    #
-    # sizes = np.ones(len(colors)) * 25  # np.square(scores / max(scores)) * 100
-    #
-    # fig = plt.figure(figsize=(4.5, 4.5))
-    #
-    # #gs = matplotlib.gridspec.GridSpec(1, 2, width_ratios=(1, 1.35))
-    #
-    # ax = fig.add_subplot(111)
-    #
-    # for m in np.unique(markers):
-    #     ax.scatter(x[markers == m], y[markers == m],
-    #                alpha=0.5, c=colors[markers == m],
-    #                s=sizes[markers == m], marker=m)
-    #
-    # ax.scatter((-1, ), (-1, ), alpha=0.5, c="C0", marker="o", label="r = .25, s = 1")
-    # ax.scatter((-1, ), (-1, ), alpha=0.5, c="C0", marker="x", label="r = .25, s = 0")
-    # ax.scatter((-1, ), (-1, ), alpha=0.5, c="C1", marker="o", label="r = .50, s = 1")
-    # ax.scatter((-1, ), (-1, ), alpha=0.5, c="C1", marker="x", label="r = .50, s = 0")
-    # plt.xlabel("Profit-based fit accuracy")
-    # plt.ylabel("Competition-based fit accuracy")
-    # ax.set_xlim(-0.02, 1.02)
-    # ax.set_ylim(-0.02, 1.02)
-    # ax.set_aspect(1)
-    # ax.legend(bbox_to_anchor=(0.65, 0.6))
-    #
-    # plt.show()
+def main(force, do_it_again, ind_profiles):
 
-    # ------------------------------------ #
-    plt.close()
+    if not os.path.exists("data/fit.p") or do_it_again or force:
+        fit_b = get_fit(force)
 
-    fig = plt.figure(figsize=(11, 5))
+    else:
+        fit_b = backup.load("data/fit.p")
+
+    if ind_profiles:
+        ind_plots(fit_b)
+
+    r_values = np.sort(np.unique(fit_b.r))
+    s_values = (False, True)
+
+    exp_conditions = list(it.product(r_values, s_values))
+
+    fig = plt.figure(figsize=(5.5, 5.5))
 
     gs = matplotlib.gridspec.GridSpec(2, 2)
 
@@ -212,51 +185,148 @@ def main(force, do_it_again):
 
     axes = []
 
-    for r_value in (0.25, 0.50):
+    for r_value, s_value in exp_conditions:
 
-        for s_value in (True, False):
+        pos = next(positions)
+        ax = fig.add_subplot(gs[pos[0], pos[1]])
 
-            pos = next(positions)
-            ax = fig.add_subplot(gs[pos[0], pos[1]])
+        cond0 = fit_b.r == r_value
+        cond1 = fit_b.display_opponent_score == int(s_value)
 
-            cond0 = fit_b.r == r_value
-            cond1 = fit_b.display_opponent_score == int(s_value)
+        cond = cond0 * cond1
 
-            cond = cond0 * cond1
+        n = np.sum(cond)
 
-            n = np.sum(cond)
+        data = np.zeros((n_dim, n))
 
-            data = np.zeros((n_dim, n))
+        for i, score in enumerate(scores_to_plot):
+            data[i] = fit_b.fit_scores[score][cond]
 
-            for i, score in enumerate(scores_to_plot):
-                data[i] = fit_b.fit_scores[score][cond]
+        # ax.set_title("r = {:.2f}, s = {}".format(r_value, int(s_value)))
+        customized_plot.violin(data=data, ax=ax, color=colors,
+                               edgecolor="black")
+        ax.set_ylim(0, 1)
+        ax.set_yticks(np.arange(0, 1.1, 0.25))
+        ax.tick_params(labelsize=8, axis="y")
+        ax.tick_params(length=0, axis='x')
+        axes.append(ax)
 
-            ax.set_title("r = {:.2f}, s = {}".format(r_value, int(s_value)))
-            customized_plot.violin(data=data, ax=ax, color=colors,
-                                   edgecolor="black")
-            ax.set_ylim(0, 1)
-            ax.set_yticks(np.arange(0, 1.1, 0.25))
-
-            ax.tick_params(length=0, axis='x')
-            axes.append(ax)
-
-    for ax in axes[:-2]:
+    for i, ax in enumerate(axes[:-2]):
+        ax.set_title(["$s = 0$", "$s = 1$"][i], fontsize=14)
         ax.set_xticklabels([])
 
     for ax in axes[-2:]:
-        ax.set_xticklabels(["Profit-maximization", "Difference-maximization"])
+        ax.set_xticklabels(["Profit\nmaximization", "Difference\nmaximization"])
 
     for ax in axes[1::2]:
         ax.set_yticklabels([])
         ax.tick_params(length=0, axis="y")
 
-    for ax in axes[0::2]:
-        ax.set_ylabel("Score")
+    for i, ax in enumerate(axes[0::2]):
+        ax.text(-0.32, 0.5, ["$r = 0.25$", "$r = 0.50$"][i], rotation="vertical", verticalalignment='center',
+                horizontalalignment='center', transform=ax.transAxes, fontsize=14)
+        ax.set_ylabel("\nScore")
 
     plt.tight_layout()
 
     plt.savefig("fig/fit.pdf")
     plt.show()
+
+    # ----------- Stats ----------------- #
+
+    r = fit_b.r
+    s = fit_b.display_opponent_score
+
+    p = fit_b.fit_scores["profit"]
+    d = fit_b.fit_scores["competition"]
+
+    to_compare = [
+        {
+            "measure": "Difference maximization",
+            "constant": "s = 0",
+            "var": "r",
+            "data": [d[(r == r_value) * (s == 0)] for r_value in (0.25, 0.50)]
+        }, {
+            "measure": "Difference maximization",
+            "constant": "s = 1",
+            "var": "r",
+            "data": [d[(r == r_value) * (s == 1)] for r_value in (0.25, 0.50)]
+        }, {
+            "measure": "Profit maximization",
+            "constant": "s = 0",
+            "var": "r",
+            "data": [p[(r == r_value) * (s == 0)] for r_value in (0.25, 0.50)]
+        }, {
+            "measure": "Profit maximization",
+            "constant": "s = 1",
+            "var": "r",
+            "data": [p[(r == r_value) * (s == 1)] for r_value in (0.25, 0.50)]
+        }, {
+            "measure": "Difference maximization",
+            "constant": "r = 0.25",
+            "var": "s",
+            "data": [d[(r == 0.25) * (s == s_value)] for s_value in (0, 1)]
+        }, {
+            "measure": "Difference maximization",
+            "constant": "r = 0.50",
+            "var": "s",
+            "data": [d[(r == 0.50) * (s == s_value)] for s_value in (0, 1)]
+        }, {
+            "measure": "Profit maximization",
+            "constant": "r = 0.25",
+            "var": "s",
+            "data": [p[(r == 0.25) * (s == s_value)] for s_value in (0, 1)]
+        }, {
+            "measure": "Profit maximization",
+            "constant": "r = 0.50",
+            "var": "s",
+            "data": [p[(r == 0.50) * (s == s_value)] for s_value in (0, 1)]
+        }
+    ]
+
+    ps = []
+    us = []
+
+    for dic in to_compare:
+        u, p = scipy.stats.mannwhitneyu(dic["data"][0], dic["data"][1])
+        ps.append(p)
+        us.append(u)
+
+    valid, p_corr, alpha_c_sidak, alpha_c_bonf = \
+        statsmodels.stats.multitest.multipletests(pvals=ps, alpha=0.01, method="b")
+
+    for p, u, p_c, v, dic in zip(ps, us, p_corr, valid, to_compare):
+        print("[Diff in {} when {} depending on {}-value] "
+              "Mann-Whitney rank test: u {}, p {:.3f}, p corr {:.3f}, significant: {}"
+              .format(dic["measure"], dic["constant"], dic["var"], u, p, p_c, v))
+        print()
+
+    table = \
+        r"\begin{table}[htbp]" + "\n" + \
+        r"\begin{center}" + "\n" + \
+        r"\begin{tabular}{llllllll}" + "\n" + \
+        r"Measure & Variable & Constant & $u$ & $p$ (before corr.) " \
+        r"& $p$ (after corr.) & Sign. at 1\% threshold \\" + "\n" + \
+        r"\hline \\" + "\n"
+
+    for p, u, p_c, v, dic in zip(ps, us, p_corr, valid, to_compare):
+
+        p = "{:.3f}".format(p) if p >= 0.001 else "$<$ 0.001"
+        p_c = "{:.3f}".format(p_c) if p_c >= 0.001 else "$<$ 0.001"
+        v = "yes" if v else "no"
+        table += r"{} & ${}$ & ${}$ & {} & {} & {} & {} \\".format(dic["measure"], dic["var"], dic["constant"], u, p, p_c, v) \
+                 + "\n"
+
+    table += \
+        r"\end{tabular}" + "\n" + \
+        r"\end{center}" + "\n" + \
+        r"\caption{Significance tests for comparison using Mann-Withney's u. Bonferroni corrections are applied.}" + "\n" + \
+        r"\label{table:significance_tests}" + "\n" + \
+        r"\end{table}"
+
+    print("*** Latex-formated table ***")
+    print(table)
+
 
 
 if __name__ == "__main__":
@@ -265,9 +335,12 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--force', action="store_true", default=False,
                         help="Re-import data")
     parser.add_argument('-d', '--do_it_again', action="store_true", default=False,
-                        help="Re-do fit")
+                        help="Re-do analysis")
+
+    parser.add_argument('-i', '--ind_profiles', action="store_true", default=False,
+                        help="Draw individual profiles")
     # parser.add_argument('-s', '--softmax', action="store_true", default=False,
     #                     help="Optmize using a softmax function")
     parsed_args = parser.parse_args()
 
-    main(force=parsed_args.force, do_it_again=parsed_args.do_it_again)
+    main(force=parsed_args.force, do_it_again=parsed_args.do_it_again, ind_profiles=parsed_args.ind_profiles)
