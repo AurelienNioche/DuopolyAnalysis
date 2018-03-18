8d0fc280b2e493628dc96ca0ff72b860aa32ba98
getz <basilegarcia@gmail.com>
2018-03-18 15:57:01 +0100

    add round_profiler.py

--- round_profiler.py
+++ round_profiler.py
@@ -4,7 +4,6 @@ import argparse
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import matplotlib.gridspec
-import operator
 
 from backup import backup
 
@@ -198,114 +197,75 @@ def individual_bar(bkup):
 
     gs = matplotlib.gridspec.GridSpec(1, 2)
 
+    pos = iter([
+        gs[0, 0],
+        gs[0, 1]
+    ])
+
     # ----------------------  0.25 --------------------- #
 
-    ax = plt.subplot(gs[0, 0])
+    for r in (0.25, 0.5):
 
-    backups = bkup.round_id[bkup.r == 0.25]
+        ax = plt.subplot(next(pos))
 
-    values = np.zeros(len(backups))
+        ids = bkup.round_id[bkup.r == r]
+        fit_scores = bkup.fit_scores[bkup.r == r]
 
-    for i, round_id in enumerate(backups):
+        values = np.zeros(len(ids))
 
-        scores = bkup.fit_scores[bkup.round_id == round_id][0]
-        score = ((scores[0] / (scores[0] + scores[1])) - 0.5) * 2
+        values = (fit_scores[:, 0] / (fit_scores[:, 0] + fit_scores[:, 1])) - 0.5
 
-        values[i] = score
+        # Set ticks
+        idx = np.argsort(values)
+        values = values[idx]
+        rd_id = ids[idx]
+        labels_pos = np.arange(len(idx))
+        ax.set_yticks(labels_pos)
+        ax.set_yticklabels(rd_id)
 
-    idx = np.argsort(values)
-    values = values[idx]
+        # Params
+        ax.tick_params(length=0, axis="y")
+        ax.set_xlim(-0.25, 0.25)
 
-    colors = ["C0" if v < 0 else "C1" for v in values]
+        # Hide frame
+        ax.spines["top"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+        ax.spines["right"].set_visible(False)
 
-    rd_id = backups[idx]
+        ax.text(
+            0.25,
+            1,
+            'Difference maximizer',
+            horizontalalignment='center',
+            verticalalignment='center',
+            transform=ax.transAxes,
+            color="C0",
+            fontweight="bold"
+        )
 
-    pos = np.arange(len(idx))
+        ax.text(
+            0.75,
+            1,
+            'Profit maximizer',
+            horizontalalignment='center',
+            verticalalignment='center',
+            transform=ax.transAxes,
+            color="C1",
+            fontweight="bold"
+        )
 
-    ax.set_yticks(pos)
-    ax.set_yticklabels(rd_id)
+        ax.set_title("r = {}".format(r), y=1.05)
 
-    ax.tick_params(length=0, axis="y")
-    ax.set_xlim(-0.5, 0.5)
-    ax.spines["top"].set_visible(False)
-    ax.spines["left"].set_visible(False)
-    ax.spines["right"].set_visible(False)
+        colors = np.zeros(len(values), dtype=(str, 2))
+        colors[:] = "C0"
+        colors[values > 0] = "C1"
 
-    ax.text(
-        0.25,
-        1,
-        'Difference maximizer',
-        horizontalalignment='center',
-        verticalalignment='center',
-        transform=ax.transAxes
-    )
+        for l, color in zip(ax.get_yticklabels(), colors):
+            l.set_color(color)
 
-    ax.text(
-        0.75,
-        1,
-        'Profit maximizer',
-        horizontalalignment='center',
-        verticalalignment='center',
-        transform=ax.transAxes
-    )
+        ax.barh(labels_pos, values, color=colors, edgecolor="white", alpha=0.8)
 
-    ax.set_title("r = .25", y=1.3)
-
-    ax.barh(pos, values, color=colors, height=0.95)
-
-    # -------- 0 5 ---------------------------------------------- #
-
-    ax = plt.subplot(gs[0, 1])
-
-    backups = bkup.round_id[bkup.r == 0.5]
-
-    values = np.zeros(len(backups))
-
-    for i, round_id in enumerate(backups):
-
-        scores = bkup.fit_scores[bkup.round_id == round_id][0]
-        score = ((scores[0] / (scores[0] + scores[1])) - 0.5) * 2
-
-        values[i] = score
-
-    idx = np.argsort(values)
-    values = values[idx]
-
-    rd_id = backups[idx]
-
-    pos = np.arange(len(idx))
-    ax.set_yticks(pos)
-    ax.set_yticklabels(rd_id)
-
-    ax.tick_params(length=0, axis="y")
-    ax.set_xlim(-0.5, 0.5)
-    ax.spines["top"].set_visible(False)
-    ax.spines["left"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-
-    ax.text(
-        0.25,
-        1,
-        'Difference maximizer',
-        horizontalalignment='center',
-        verticalalignment='center',
-        transform=ax.transAxes
-    )
-
-    ax.text(
-        0.75,
-        1,
-        'Profit maximizer',
-        horizontalalignment='center',
-        verticalalignment='center',
-        transform=ax.transAxes
-    )
-
-    colors = ["C0" if v < 0 else "C1" for v in values]
-
-    ax.set_title("r = .5", y=2)
-    ax.barh(pos, values, color=colors, height=0.95)
-
+    plt.tight_layout()
     plt.show()
 
 
