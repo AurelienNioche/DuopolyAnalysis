f98e44de33addf966407e14dc52e08b75deec952
AurelienNioche <nioche.aurelien@gmail.com>
2019-01-03 18:40:36 +0200

    update

--- simulation/parameters.py
+++ simulation/parameters.py
@@ -91,7 +91,7 @@ def get_last_random_parameters(json_file):
     return i
 
 
-def load(json_file, force_params, random):
+def load(json_file, force_params, random=False):
 
     if not os.path.exists(json_file) and random or force_params:
 
